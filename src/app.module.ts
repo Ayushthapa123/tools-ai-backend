e97d8d0d86f8f99ed98a8a17e9ef064f8279ca7f
Ayush Thapa <thapaaayush115@gmail.com>
2024-02-26 06:57:15 +0545

    fix: fix error on main page

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-
 import { join } from 'path';
 
 import { PrismaService } from './prisma/prisma.service';
